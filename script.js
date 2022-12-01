e061b29738212fcf053a9d02b8fae2b6c7c46b47
WinserEX <winser500@gmail.com>
2022-12-01 00:53:20 -0400

    User input ready

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-console.log('Certificates');
+let form = document.querySelector('#form');
 
-//Member
-let member = document.querySelector('#member');
-let p1 = document.createElement('P');
-p1.innerHTML = "Fulanito de Tal";
-member.append(p1);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //Member
+    let member = document.querySelector('#member');
+    let p1 = document.createElement('P');
+    p1.innerHTML = form.elements.memberQuery.value;
+    member.append(p1);
 
-//Points
-let points = document.querySelector('#points');
-points.innerHTML = "500";
+    //Points
+    let points = document.querySelector('#points');
+    points.innerHTML = form.elements.pointsQuery.value;
 
-//Contract ID
-let contractID = document.querySelector('#contractID');
-let p3 = document.createElement('P');
-p3.innerHTML = 950095956;
-contractID.append(p3);
+    //Contract ID
+    let contractID = document.querySelector('#contractID');
+    let p3 = document.createElement('P');
+    p3.innerHTML = form.elements.contractidQuery.value;
+    contractID.append(p3);
+
+    //Sales Floor
+    let salesFloor = document.querySelector('#salesFloor');
+    let p4 = document.createElement('P');
+    p4.innerHTML = form.elements.approvedbyQuery.value;
+    salesFloor.append(p4);
+
+    //Approved by
+    let approvedBy = document.querySelector('#approvedBy')
+    let p5 = document.createElement('P');
+    p5.innerHTML = form.elements.salesfloorQuery.value;
+    approvedBy.append(p5);
+    
+})
 
 
-//Sales Floor
-let salesFloor = document.querySelector('#salesFloor');
-let p4 = document.createElement('P');
-p4.innerHTML = "Dominicus";
-salesFloor.append(p4);
 
-//Approved by
-let approvedBy = document.querySelector('#approvedBy')
-let p5 = document.createElement('P');
-p5.innerHTML = "Winser Espinal";
-approvedBy.append(p5);
 
 //Date
 let date = document.querySelector('#date')
